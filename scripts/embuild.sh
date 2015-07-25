fbe4635764f0538e2db89ad8318de2ba3dafd31f
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-07-25 17:07:12 +0300

    added simplified api

---
+++ scripts/embuild.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+set -e
+
+JPEG_NAME="jpeg-9a"
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+ROOT_DIR=$(readlink -f "${SCRIPT_DIR}/../")
+JPEG_DIR=$(readlink -f "${SCRIPT_DIR}/../../${JPEG_NAME}")
+
+# print an error
+function fatal {
+  echo "$0:" "$@" >&2
+  exit 1
+}
+
+
+function usage {
+  script=$(basename "$0")
+  echo "Usage: $script [OPTIONS]
+
+Compiles libjpeg and libjpegasm into JavaScript.
+
+Examples:
+  $script --lib=jpeg --configure --make
+    - Configure & make libjpeg.
+
+  $script --lib=jpegasm
+    - Configure & make libjpegasm.
+
+Options:
+    -h|--help       Display this help message.
+    -l=*|--lib=*    Library to process: jpeg | jpegasm.
+    -c|--configure  Configure the library.
+    -m|--make       Make the library.
+"
+}
+
+function configure_jpeg {
+  (cd ${JPEG_DIR}; emconfigure ./configure)
+}
+
+function make_jpeg {
+  (cd ${JPEG_DIR}; emmake make)
+}
+
+# build libjpeg
+function jpeg {
+  local is_confugure=$1
+  local is_make=$2
+
+  if [[ $is_confugure -eq 1 ]]; then
+    configure_jpeg
+  fi
+
+  if [[ $is_make -eq 1 ]]; then
+    make_jpeg
+  fi
+}
+
+function jpegasm_compile {
+set -x
+  pushd ${SCRIPT_DIR}
+
+  EMCC=emcc
+  CFLAGS="-std=c11"
+
+  JPEG_SO_PATH=../../${JPEG_NAME}/.libs/libjpeg.so
+
+  mkdir -p ${ROOT_DIR}/build
+  cd ${ROOT_DIR}/build
+  ${EMCC} ${CFLAGS} -Wl,-l${JPEG_SO_PATH} ../jpegasm/api.c -I../../${JPEG_NAME} -o jpegasm.bc
+  ${EMCC} ${CFLAGS} ${JPEG_SO_PATH} jpegasm.bc -s EXPORTED_FUNCTIONS=@../scripts/exported_functions -o jpegasm.js
+
+  popd
+
+set +x
+}
+
+# build libjpegasm
+function jpegasm {
+  jpegasm_compile
+}
+
+# Build the specified library
+function lib {
+  local libname=$1
+
+  if [[ $1 =~ ^(jpeg|jpegasm)$ ]]; then
+    if [[ $1 == "jpeg" ]]; then
+      jpeg $2 $3
+    else
+      jpegasm
+    fi
+  else
+    fatal "Invalid library: $1"
+  fi
+}
+
+# if there are no arguments, print usage info
+if [ $# -eq 0 ]; then
+  usage
+  exit
+fi
+
+
+LIBNAME=
+CONFIGURE=0
+MAKE=0
+
+for i in "$@"
+do
+  case $i in
+      -l=*|--lib=*)
+      LIBNAME="${i#*=}"
+      shift
+      ;;
+      -c|--configure)
+      CONFIGURE=1
+      shift
+      ;;
+      -m|--make)
+      MAKE=1
+      shift
+      ;;
+      -h|--help)
+      usage
+      shift
+      ;;
+      --default)
+      shift
+      ;;
+      *)
+      ;;
+  esac
+done
+
+lib ${LIBNAME} ${CONFIGURE} ${MAKE}
---
+++ test/api-spec.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var should = require('should');
+var Module = require('./../build/jpegasm');
+var fs = require('fs');
+
+describe('JpegAsm', function () {
+
+  var rgbWidth = 32;
+  var rgbHeight = 32;
+  var rgbBuffer = new ArrayBuffer(rgbWidth * rgbHeight * 3);
+
+  function initRgbImage() {
+    var view = new Uint8Array(rgbBuffer);
+    var index;
+    for (var y = 0; y != rgbHeight; ++y) {
+      for (var x = 0; x != rgbWidth * 3; x += 3) {
+        index = y * rgbWidth * 3 + x;
+        view[index + 0] = 0x00; // R
+        view[index + 1] = 0x00; // G
+        view[index + 2] = 0xFF; // B
+      }
+    }
+  }
+
+  function writeJpeg(fileName) {
+
+  }
+
+  before(function () {
+    initRgbImage();
+  });
+
+  // wrap functions
+  var encodeJpeg = Module.cwrap('encode_jpeg', undefined, ['number', 'number', 'number', 'number', 'number', 'number']);
+  var decodeJpeg = Module.cwrap('decode_jpeg', undefined, ['number', 'number', 'number', 'number', 'number']);
+
+
+  it('encodes JPEG', function () {
+    var quality = 80;
+
+    var nDataBytes = rgbBuffer.byteLength;
+    var dataPtr = Module._malloc(nDataBytes);
+    var dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    dataHeap.set(new Uint8Array(rgbBuffer));
+
+    var jpegBufferPtrPtr = Module._malloc(Uint32Array.BYTES_PER_ELEMENT);
+    var jpegBufferSizePtr = Module._malloc(Uint32Array.BYTES_PER_ELEMENT * 2);
+
+    encodeJpeg(dataPtr, rgbWidth, rgbHeight, quality, jpegBufferPtrPtr, jpegBufferSizePtr);
+
+    var jpegBufferSize = Module.getValue(jpegBufferSizePtr, 'i64');
+    var jpegBufferPtr = Module.getValue(jpegBufferPtrPtr, 'i32');
+
+    var jpegBuffer = new Uint8Array(Module.HEAPU8.buffer, jpegBufferPtr, jpegBufferSize);
+
+    fs.writeFileSync('encoded-by-js.jpeg', new Buffer(jpegBuffer));
+
+    Module._free(dataPtr);
+    Module._free(jpegBufferPtrPtr);
+    Module._free(jpegBufferSizePtr);
+
+    jpegBufferSize.should.be.greaterThan(0);
+    jpegBufferPtr.should.be.greaterThan(0);
+  });
+
+  it.only('decodes JPEG', function () {
+
+    var jpegBuffer = fs.readFileSync(__dirname + '/data/sample.jpg');
+
+    var nDataBytes = jpegBuffer.length;
+    var dataPtr = Module._malloc(nDataBytes);
+    var dataHeap = new Uint8Array(Module.HEAPU8.buffer, dataPtr, nDataBytes);
+    for(var i = 0; i != nDataBytes; ++i) {
+      dataHeap[i] = jpegBuffer[i];
+    }
+
+    var outBufferPtrPtr = Module._malloc(Uint32Array.BYTES_PER_ELEMENT);
+    var outBufferWidthPtr = Module._malloc(Uint32Array.BYTES_PER_ELEMENT);
+    var outBufferHeightPtr = Module._malloc(Uint32Array.BYTES_PER_ELEMENT);
+
+    decodeJpeg(dataPtr, nDataBytes, outBufferPtrPtr, outBufferWidthPtr, outBufferHeightPtr);
+
+    var outBufferPtr = Module.getValue(outBufferPtrPtr, 'i32');
+    var outBufferWidth = Module.getValue(outBufferWidthPtr, 'i32');
+    var outBufferHeight = Module.getValue(outBufferHeightPtr, 'i32');
+
+    outBufferPtr.should.be.greaterThan(0);
+    outBufferWidth.should.be.equal(32);
+    outBufferHeight.should.be.equal(32);
+
+    //var rgbHeap = new Uint8Array(Module.HEAPU8.buffer, outBufferPtr, outBufferWidth * outBufferHeight * 3);
+
+    Module._free(dataPtr);
+    Module._free(outBufferPtrPtr);
+    Module._free(outBufferWidthPtr);
+    Module._free(outBufferHeightPtr);
+  });
+});
+
+/*
+ var buf = Module._malloc(myTypedArray.length*myTypedArray.BYTES_PER_ELEMENT);
+ Module.HEAPU8.set(myTypedArray, buf);
+ Module.ccall('my_function', 'number', ['number'], [buf]);
+ Module._free(buf);
+ */
