2e6dd92ff87b4445f3d5eb9b6421f966cb0cc764
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-07-27 18:59:36 +0300

    wrapped in an universal module

--- jpegasm/release-api-post.js
+++ jpegasm/release-api-post.js
@@ -1,3 +1,2 @@
 return API;
-})();
-module['exports'] = API;
+}));
--- jpegasm/release-api-pre.js
+++ jpegasm/release-api-pre.js
@@ -1,1 +1,10 @@
-var API = (function () {
+(function (root, factory) {
+  if (typeof define === 'function' && define['amd']) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module['exports'] = factory();
+  } else {
+    root['jpegasm'] = factory();
+  }
+}(this, function () {
+  var API = {};
